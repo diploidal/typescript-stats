6b394461244d4c11d7289d2c74fc4910ba7792ae
Diploidal <diploidal@gmail.com>
2021-11-09 16:14:28 +0100

    simple data analysis based on conditions with result print

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,15 @@ const matches = fs
     return row.split(',');
   });
 
-console.log(matches);
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if (
+    (match[1] === 'Man United' && match[5] === 'H') ||
+    (match[2] === 'Man United' && match[5] === 'A')
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Manchester United won ${manUnitedWins} games.`);
