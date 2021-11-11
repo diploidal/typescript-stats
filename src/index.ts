276754494556be761b683b66e9d33c801a79e8a6
Diploidal <diploidal@gmail.com>
2021-11-11 21:32:19 +0100

    moved enum to new file, prepared date conversion to Date, parsed result data properly

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import { CsvFileReader } from './CsvFileReader';
+import { MatchResult } from './matchResult';
 
 const reader = new CsvFileReader('football.csv');
 reader.read();
-
-// Enum (enumeration) usage
-enum MatchResult {
-  HomeWin = 'H',
-  AwayWin = 'A',
-  Draw = 'D',
-}
 
 let manUnitedWins = 0;
 
